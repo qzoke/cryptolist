0a68fc3650ad41cfa6da05dee7ce835e66f5fd91
Cameron Ray <cameron@cameronray.net>
2018-07-02 16:16:36 -0400

    Add search button, remove search on keyUp

--- src/client/scenes/home/scenes/crypto-list-grid/components/search.jsx
+++ src/client/scenes/home/scenes/crypto-list-grid/components/search.jsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Input } from 'reactstrap';
+import { Input, Form, FormGroup, Button, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
-export const Search = ({ updateQuery }) => {
-  let changeSearchQuery = event => {
-    let search = event.target.value;
+export class Search extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.changeSearchText = this.changeSearchText.bind(this);
+    this.changeSearchQuery = this.changeSearchQuery.bind(this);
+    this.state = {
+      searchText: '',
+    };
+  }
+
+  changeSearchText(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  changeSearchQuery(event) {
+    event.preventDefault();
+    let search = this.state.searchText;
     let query = null;
     if (search.length) {
       query = {
@@ -18,11 +33,25 @@ export const Search = ({ updateQuery }) => {
         ],
       };
     }
-    updateQuery(query);
-  };
+    this.props.updateQuery(query);
+  }
 
-  return <Input type="text" placeholder="Search" onChange={changeSearchQuery} />;
-};
+  render() {
+    return (
+      <Form className="search" onSubmit={this.changeSearchQuery}>
+        <Input
+          type="text"
+          placeholder="Search"
+          name="searchText"
+          onChange={this.changeSearchText}
+        />
+        <Button color="link">
+          <FontAwesomeIcon icon="search" />
+        </Button>
+      </Form>
+    );
+  }
+}
 
 Search.propTypes = {
   updateQuery: PropTypes.func,
