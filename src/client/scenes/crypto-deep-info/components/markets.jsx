aa1d506c8918d70cbd2ab69e4458f8294aad5c5f
Cameron Ray <cameron@cameronray.net>
2018-09-04 18:01:09 -0400

    Clean up markets page

--- src/client/scenes/crypto-deep-info/components/markets.jsx
+++ src/client/scenes/crypto-deep-info/components/markets.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {} from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Loading } from '../../../components/loading';
-import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from 'recharts';
 import { Query } from 'regraph-request';
 
 const colors = ['#EE8434', '#335C67', '#A33B20', '#EDD382', '#306B34'];
@@ -54,6 +54,27 @@ export class MarketsComponent extends React.Component {
     }, []);
   }
 
+  renderCustomizedLabel({ cx, cy, midAngle, innerRadius, outerRadius, percent }) {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    if (percent * 100 > 10)
+      return (
+        <text
+          x={x}
+          y={y}
+          fill="white"
+          textAnchor={x > cx ? 'start' : 'end'}
+          dominantBaseline="central"
+        >
+          {`${(percent * 100).toFixed(0)}%`}
+        </text>
+      );
+    return null;
+  }
+
   render() {
     if (!this.props.data.currency) return <Loading />;
     if (!this.props.data.currency.markets) return <div />;
@@ -65,56 +86,65 @@ export class MarketsComponent extends React.Component {
       name: volume[0],
       value: volume[1],
     }));
+    let totalExchangeVolume = exchangeVolumes.reduce((acc, v) => v.value + acc, 0);
+    exchangeVolumes.sort((a, b) => (a.value > b.value ? 1 : -1));
 
     let quoteVolumes = Object.entries(quoteVolumeObjects).map(volume => ({
       name: volume[0],
       value: volume[1],
     }));
+    quoteVolumes.sort((a, b) => (a.value > b.value ? 1 : -1));
 
     return (
       <div className="currency-info-container markets">
         <div className="volume-market pie">
           <h4>Volume / Exchange</h4>
           <div className="market-container">
-            <PieChart width={300} height={300}>
-              <Pie
-                cx={150}
-                cy={150}
-                data={exchangeVolumes}
-                dataKey="value"
-                nameKey="name"
-                innerRadius="50%"
-                outerRadius="80%"
-                label={item => item.name}
-              >
-                {exchangeVolumes.map((entry, index) => (
-                  <Cell key={entry.name} fill={colors[index % colors.length]} />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={exchangeVolumes}
+                  dataKey="value"
+                  nameKey="name"
+                  innerRadius="40%"
+                  outerRadius="80%"
+                  label={this.renderCustomizedLabel}
+                  labelLine={false}
+                >
+                  {exchangeVolumes.map((entry, index) => (
+                    <Cell key={entry.name} fill={colors[index % colors.length]} />
+                  ))}
+                </Pie>
+                <Tooltip
+                  formatter={value => `${(value / totalExchangeVolume * 100).toFixed(2)}%`}
+                />
+              </PieChart>
+            </ResponsiveContainer>
           </div>
         </div>
         <div className="volume-market pie">
           <h4>Volume / Quote</h4>
-          <div style={{ height: '21em' }}>
-            <PieChart width={300} height={300}>
-              <Pie
-                cx={150}
-                cy={150}
-                data={quoteVolumes}
-                dataKey="value"
-                nameKey="name"
-                innerRadius="50%"
-                outerRadius="80%"
-                label={item => item.name}
-              >
-                {exchangeVolumes.map((entry, index) => (
-                  <Cell key={entry.name} fill={colors[index % colors.length]} />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
+          <div className="market-container">
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={quoteVolumes}
+                  dataKey="value"
+                  nameKey="name"
+                  innerRadius="40%"
+                  outerRadius="80%"
+                  labelLine={false}
+                  label={this.renderCustomizedLabel}
+                >
+                  {exchangeVolumes.map((entry, index) => (
+                    <Cell key={entry.name} fill={colors[index % colors.length]} />
+                  ))}
+                </Pie>
+                <Tooltip
+                  formatter={value => `${(value / totalExchangeVolume * 100).toFixed(2)}%`}
+                />
+              </PieChart>
+            </ResponsiveContainer>
           </div>
         </div>
       </div>
